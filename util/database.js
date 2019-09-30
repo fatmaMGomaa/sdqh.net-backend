e93426582c564773d1cb05d2561ff7160b2e2874
fatmaMGomaa <fatma-mohamed@feps.edu.eg>
2019-09-30 14:17:27 +0200

    database

--- util/database.js
+++ util/database.js
@@ -1,10 +1,20 @@
 const Sequelize = require('sequelize');
 
-const { DB_NAME, DB_USER, SECRET } = process.env;
-
-const sequelize = new Sequelize(DB_NAME, DB_USER, SECRET, {
-    dialect: 'postgres',
-    host: 'localhost'
-});
-
+const { DB_NAME, DB_USER, SECRET, DATABASE_URL } = process.env;
+let sequelize;
+if (process.env.DATABASE_URL) {
+    // the application is executed on Heroku ... use the postgres database
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        protocol: 'postgres',
+        dialectOptions: {
+            ssl: true
+        }
+    })
+} else {
+    sequelize = new Sequelize(DB_NAME, DB_USER, SECRET, {
+        dialect: 'postgres',
+        host: 'localhost'
+    });
+}
 module.exports = sequelize;
